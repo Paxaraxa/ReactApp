f12b3a65ae19f36f40e25ad032bfbe7d396cb387
Alejo Olivero <oterolivero@gmail.com>
2021-03-08 17:48:41 -0300

    Generate automatic html with react

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,35 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{Component} from "react"
-class App extends Component {
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+class App extends React.Component {
   constructor() {
     super();
-    this.state = [
-      {name:"Frankestein"},
-      {name:"test2"},
-      {name:"test3"}
-    ]
-      
+    this.state = {
+      monsters: [
+        {
+          name: "Frankestein",
+          id: "1",
+        },
+        {
+          name: "Dracula",
+          id: "2",
+        },
+
+        {
+          name: "Retutu",
+          id: "3",
+        },
+      ],
+    };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-    <p>{this.state.string}</p>
-    <button onClick={()=> this.setState({"string":"holanda"})}>Change text</button>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
