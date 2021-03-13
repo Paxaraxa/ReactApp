d91ef90baf8f17472182ca60723825f09db22348
Alejo Olivero <oterolivero@gmail.com>
2021-03-13 17:25:52 -0300

    obtain data dinamic

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -22,10 +22,18 @@ class App extends React.Component {
       ],
     };
   }
+  //fetch new data
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json()) //obtain the response and convert to json
+      .then((users) => this.setState({ monsters: users })); //set de data of the usrs to the monsters array
+  }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
+        {this.state.monsters.map((
+          monster //iterate in all the occurencies of the monster array in the state object
+        ) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
       </div>
